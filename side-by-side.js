eb3b5512fbdfdb37079888dd49235cd2a0aa30ce
UruBad <badtiev@list.ru>
2016-05-15 13:28:04 +0300

    Update side-by-side.js

--- side-by-side.js
+++ side-by-side.js
@@ -5,7 +5,8 @@ var flexslider = null;
 var isTension = true;
 
 $(function () {
-    $(".side-by-side-button").click(function () {
+    $('<div><button class="sqs-suppress-edit-mode sqs-editable-button" id="side-by-side-button"><div class="sqs-add-to-cart-button-inner" id="yui_3_17_2_3_1463297059130_2963">Compare side-by-side</div></button></div>').insertAfter($(".sqs-add-to-cart-button-wrapper"));
+    $("#side-by-side-button").click(function () {
         $("#side-by-side").fadeIn("slow");
         startSize = $("#size").val();
         UpdateSizes(startSize);
