8685e9e7d2d07970ad10ca29f54fd4670d1390dd
UruBad <badtiev@list.ru>
2016-06-09 17:31:07 +0300

    Update product.js

--- product.js
+++ product.js
@@ -59,7 +59,7 @@ var select_size;
 	}
 	contentSmall += '<div class="slide video_view"><a class="catwalk catwalk_movie" href="#" title="View catwalk" style="margin-left:16px;" onclick="openPopup();return false;"></a></div>';
 	contentSmall += "</div>";
-	contentSmall += '<div class="buttons"><div><button class="sqs-suppress-edit-mode sqs-editable-button" id="add-to-favorites"><div class="sqs-add-to-cart-button-inner" id="yui_3_17_2_3_1463297059130_2963">Add to Dressing Room</div></button></div></div>";
+	contentSmall += '<div class="buttons"><div><button class="sqs-suppress-edit-mode sqs-editable-button" id="add-to-favorites"><div class="sqs-add-to-cart-button-inner">Add to Dressing Room</div></button></div></div>';
 	contentSmall += "</div>";
 	$(contentSmall).insertBefore($(".product-quantity-input"));
 	$("#productThumbnails").append(content);
