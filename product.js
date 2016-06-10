663993f46a372b0c8a48d5c6b31e8cf4b8768856
UruBad <badtiev@list.ru>
2016-06-10 16:59:03 +0300

    Update product.js

--- product.js
+++ product.js
@@ -50,7 +50,7 @@ var select_size;
 	var counter = 1;
 	for (var i = 1; i <= 6; i++)
         {
-        	contentSmall += '<div class="slide dop_slide" data-target="' + counter + '"><img src="" /><div class=""></div></div>';
+        	contentSmall += '<div class="slide dop_slide" data-target="' + counter + '"><a href="#"><img src="" /></a><div class=""></div></div>';
         	content += '<div class="slide trimirror_slide click_event content-fill" data-target="' + counter + '"><img src="http://static1.squarespace.com/static/573ff47b2eeb81d00cc8aea3/t/574e759ab6aa6043148aa31a/1464759706766/spin.gif" data-load="false" data-src="" data-image="" data-image-dimensions="373x585" data-image-focal-point="0.5,0.5" alt=""><div class=""></div></div>';
         	contentBig += '<div class="slide trimiror_big_slide" data-target="' + counter + '"><img class="image_big" data-load="false" data-src="" data-image="" data-image-dimensions="500x910" data-image-focal-point="0.5,0.5" alt=""><div class=""></div></div>';
                 counter++;
@@ -128,8 +128,8 @@ var select_size;
         	}
         });
         
-        $(".dop_slide").click(function(){
-        	var target = $(this).attr("data-target");
+        $(".dop_slide a").click(function(){
+        	var target = $(this)parent().attr("data-target");
         	$(".click_event[data-target='" + target + "']").click();
         	return false;
         });
