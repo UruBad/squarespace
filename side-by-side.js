556e644938ee84de91d55dbe142756724c78f0f0
UruBad <badtiev@list.ru>
2016-06-10 15:56:35 +0300

    Update side-by-side.js

--- side-by-side.js
+++ side-by-side.js
@@ -19,6 +19,8 @@ function UpdateSideBySide() {
         if (updateSize != startSize) {
             $("select[data-variant-option-name='Size']").val(updateSize);
             $("select[data-variant-option-name='Size']").trigger("change");
+            console.log($("select[data-variant-option-name='Size'] option:selected"));
+            $("select[data-variant-option-name='Size'] option:selected").click();
             $("select[data-variant-option-name='Size']").trigger("select");
         }
         return false;
