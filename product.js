a8cfbfe468dddd49066d7708f8e5017d4bb742db
UruBad <badtiev@list.ru>
2016-06-13 12:45:48 +0300

    Update product.js

--- product.js
+++ product.js
@@ -148,17 +148,23 @@ var select_size;
         	} else if(!$(this).hasClass("video_view")){
         		$(".legend").hide();
         		$("#trimirror_logo").hide();
-        		if ((navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) || navigator.userAgent.indexOf("MSIE ") != -1) {
+        		if ((navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1)) {
         			$("#productSlideshow .trimiror_big_slide").animate({ opacity: "0", filter: "alpha(opacity=0)" }, 500);
+        		} else if(navigator.userAgent.indexOf("MSIE ") != -1){
+        			$("#productSlideshow .trimiror_big_slide").fadeOut(500);
         		}
         	}
         });
 
         $(".dop_slide a").click(function(){
         	var target = $(this).parent().attr("data-target");
-        	if ((navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) || navigator.userAgent.indexOf("MSIE ") != -1) {
+        	if ((navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1)) {
         		$("#productSlideshow .slide").animate({ opacity: "0", filter: "alpha(opacity=0)" }, 500);
         		$(".slide.trimiror_big_slide[data-target='" + target + "']").stop().animate({ opacity: "100", filter: "alpha(opacity=100)" }, 500);
+        	} else if(navigator.userAgent.indexOf("MSIE ") != -1){
+        		$("#productSlideshow .slide").fadeOut(500, funciton(){
+        			$(".slide.trimiror_big_slide[data-target='" + target + "']").fadeIn(500);
+        		});
         	}
        		$(".click_event[data-target='" + target + "']").trigger("click");
         	return false;
