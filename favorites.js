5f6e36eac65855732225ca80d6c18627ea1791fd
UruBad <badtiev@list.ru>
2016-06-21 11:13:44 +0300

    Update favorites.js

--- favorites.js
+++ favorites.js
@@ -110,7 +110,7 @@ $(function () {
 			}		    	
 
 		    var shotUrl = "https://widget.trimirror.com/GetShot?clientId=" + clientId + "&code=#code#&color=#color#&size=#size#&extcode=#extcode#&extcolor=#extcolor#&extsize=#extsize#&width=#width#&height=#height#&view=1&userId=#userId#&jpg";
-		    var blockWidth = $(".sqs-block-content").width();
+		    var blockWidth = $("#dressing_slides").width();
 		    var width = Math.ceil(blockWidth / 2) - 10;
 		    shotUrl = shotUrl.replace("#userId#", GetUserId()).replace("#width#", width).replace("#height#", Math.ceil(width * 1.5));
 		    var extItemShot = false
@@ -125,7 +125,8 @@ $(function () {
 		    			break;
 		    		}
 		    	}
-		    }			
+		    }	
+		    	alert(shotUrl);
 			shotUrl = shotUrl.replace("#extcode#", "").replace("#extsize#", "").replace("#extcolor#", "");
 			shareLink = shareLink.replace("#link#", encodeURIComponent(window.location.href)).replace("#items#", itemsNames.substring(0, itemsNames.length - 1)) + '&img=' + shotUrl;
 			html +=
