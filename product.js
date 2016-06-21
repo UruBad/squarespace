6904cb3b7f135c1855eefc31ee832834db2e5fc6
UruBad <badtiev@list.ru>
2016-06-21 09:52:28 +0300

    Update product.js

--- product.js
+++ product.js
@@ -292,7 +292,7 @@ function AddToFavorites(){
 		var imgUrl = $("#productThumbnails img").first().attr("data-image");
 		$.ajax({
 			url: addToFavorites,
-			data: "code=" + code + "&color=" + color + "&size=" + size + "&userId=" + GetUserId() + "&title=" + encodeURI(title) + "&price=" + price + "&imgUrl=" + encodeURI(imgUrl) + "&productUrl=" + encodeURI(window.location.href + "#size=" + size),
+			data: "code=" + code + "&color=" + color + "&size=" + size + "&userId=" + GetUserId() + "&title=" + encodeURI(title) + "&price=" + price + "&imgUrl=" + encodeURI(imgUrl) + "&productUrl=" + encodeURI(window.location.href + "?size=" + size),
 			method: "post",
 			dataType: "jsonp",
 			success: UpdateFavorites,
